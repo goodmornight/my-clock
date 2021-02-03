cb9f331655d0c6b31a0667b3571cdedcb5b5d571
GoodNight <goodnightlilanhui@gmail.com>
2021-02-03 11:29:48 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      date: new Date()
+    }
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(() => this.tick(),1000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID)
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    })
+  }
+
+  render(){
+    let date = this.state.date;
+    let y = date.getFullYear();
+    let m = date.getMonth() + 1;
+    let d = date.getDate();
+    return (
+      <div>
+        <h1>{y + '年' + m + '月' + d + '日'}</h1>
+        <h1>{date.toLocaleTimeString()}</h1>
+      </div>
+      
+      )
+  }
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Clock />
       </header>
     </div>
   );
