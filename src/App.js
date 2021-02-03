e9e019dd425d47d816c474b251aae3c650030f68
GoodNight <goodnightlilanhui@gmail.com>
2021-02-03 14:26:32 +0800

    feat: github logo & github ci

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import './App.css';
+import imgUrl from './assets/images/GitHub.svg';
+const config = {
+  github: "https://github.com/goodmornight",
+  // bilibili: "https://space.bilibili.com/281934317",
+  // zhihu: "https://www.zhihu.com/people/li-lan-hui-29",
+  // juejin: "https://juejin.cn/user/1169536103495064",
+}
 
 class Clock extends React.Component {
+
   constructor(props) {
     super(props)
     this.state = {
@@ -33,16 +41,28 @@ class Clock extends React.Component {
         <h1>{y + '年' + m + '月' + d + '日'}</h1>
         <h1>{date.toLocaleTimeString()}</h1>
       </div>
-      
       )
   }
 }
 
+function Media() {
+
+  return (
+    <div>
+      <a target="_blank" href={config.github}>
+        <img src={imgUrl} />
+      </a>
+    </div>
+  )
+}
+
 function App() {
+  Media()
   return (
     <div className="App">
       <header className="App-header">
         <Clock />
+        <Media />
       </header>
     </div>
   );
